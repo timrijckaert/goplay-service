c28e1ecb00afcb8bcb4d24704d12fc8545dba76b
Tim Rijckaert <tim.rijckaert@bnpparibasfortis.com>
2020-11-07 20:26:10 +0100

    Refactor the code

--- vrtnu/src/main/java/be/tapped/vrtnu/Main.kt
+++ vrtnu/src/main/java/be/tapped/vrtnu/Main.kt
@@ -1,7 +1,14 @@
 package be.tapped.vrtnu
 
+import be.tapped.vrtnu.model.VRTLogin
+
 fun main(args: Array<String>) {
     val userName = args[0]
     val password = args[1]
-    println(TokenResolver.login(userName, password))
+    val tokenResolver = TokenResolver(object : TokenResolver.Listener {
+        override fun onFailedToLogin(failure: VRTLogin.Failure) {
+
+        }
+    })
+    tokenResolver.login(userName, password)
 }
--- vrtnu/src/main/java/be/tapped/vrtnu/TokenResolver.kt
+++ vrtnu/src/main/java/be/tapped/vrtnu/TokenResolver.kt
@@ -3,63 +3,69 @@ package be.tapped.vrtnu
 import be.tapped.vrtnu.model.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.*
-import okhttp3.FormBody
+import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import java.lang.Exception
 
-object TokenResolver {
+object DefaultCookieJar : CookieJar {
+    private val cookieCache: MutableMap<String, List<Cookie>> = mutableMapOf()
 
-    private const val API_KEY = "3_qhEcPa5JGFROVwu5SWKqJ4mVOIkwlFNMSKwzPDAh8QZOtHqu6L4nD5Q7lk0eXOOG"
-    private const val LOGIN_URL = "https://accounts.vrt.be/accounts.login"
-    private const val TOKEN_GATEWAY_URL = "https://token.vrt.be"
+    override fun loadForRequest(url: HttpUrl): List<Cookie> = cookieCache[url.toString()] ?: emptyList()
+
+    override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+        cookieCache[url.toString()] = cookies
+    }
+}
+
+class TokenValidator {
+    fun isValidToken(loginJson: JsonObject): Boolean =
+        loginJson["errorCode"]!!.jsonPrimitive.int == 0
+
+    fun handleInvalidToken(listener: TokenResolver.Listener, loginJson: JsonObject) {
+        listener.onFailedToLogin(
+            when (loginJson["errorDetails"]?.jsonPrimitive?.content) {
+                "invalid loginID or password" -> VRTLogin.Failure.InvalidCredentials
+                "loginID must be provided" -> VRTLogin.Failure.MissingLoginId
+                "Missing required parameter: password" -> VRTLogin.Failure.MissingPassword
+                else -> VRTLogin.Failure.Unknown
+            }
+        )
+    }
+}
+
+class TokenResolver(
+    private val listener: Listener,
+    private val tokenValidator: TokenValidator = TokenValidator()
+) {
+
+    interface Listener {
+        fun onFailedToLogin(failure: VRTLogin.Failure)
+    }
+
+    companion object {
+        private const val API_KEY =
+            "3_qhEcPa5JGFROVwu5SWKqJ4mVOIkwlFNMSKwzPDAh8QZOtHqu6L4nD5Q7lk0eXOOG"
+        private const val LOGIN_URL = "https://accounts.vrt.be/accounts.login"
+        private const val TOKEN_GATEWAY_URL = "https://token.vrt.be"
+    }
 
     private val jsonMediaType = "application/json; charset=utf-8".toMediaType()
 
-    private val client = OkHttpClient()
+    private val client = OkHttpClient.Builder()
+        .cookieJar(DefaultCookieJar)
+        .build()
 
-    fun login(userName: String, password: String): VRTLogin =
-        try {
-            val loginJson = getLoginJson(userName, password)
-            if (loginJson["errorCode"]!!.jsonPrimitive.int != 0) {
-                when (loginJson["errorDetails"]?.jsonPrimitive?.content) {
-                    "invalid loginID or password" -> VRTLogin.Failure.InvalidCredentials
-                    "loginID must be provided" -> VRTLogin.Failure.MissingLoginId
-                    "Missing required parameter: password" -> VRTLogin.Failure.MissingPassword
-                    else -> VRTLogin.Failure.Unknown
-                }
-            } else {
-                val vrtToken = getNewToken("X-VRT-Token", userName, loginJson)
-                if (vrtToken != null) {
-                    VRTLogin.Success.OK(
-                        Token(
-                            VRTNUToken("erueru"),
-                            VRTProfileToken("erer"),
-                            RefreshToken("rueurg"),
-                            2354345465
-                        )
-                    )
-                } else {
-                    VRTLogin.Failure.Unknown
-                }
-            }
-        } catch (e: Exception) {
-            VRTLogin.Failure.Exceptionally(e)
+    fun login(userName: String, password: String) {
+        val loginJson = getLoginJson(userName, password)
+        if (tokenValidator.isValidToken(loginJson)) {
+            fetchXVRTToken(userName, loginJson)
+        } else {
+            tokenValidator.handleInvalidToken(listener, loginJson)
         }
+    }
 
-    private fun getNewToken(
-        name: String,
-        userName: String,
-        loginJson: JsonObject
-    ): Pair<VRTNUToken, String>? =
-        when (name) {
-            "X-VRT-Token" -> getXVRTToken(userName, loginJson)
-            else -> throw IllegalArgumentException("Not handled $name")
-        }
-
-    private fun getXVRTToken(userName: String, loginJson: JsonObject): Pair<VRTNUToken, String>? {
+    private fun fetchXVRTToken(userName: String, loginJson: JsonObject) {
         val loginToken =
             loginJson["sessionInfo"]!!.jsonObject["login_token"]?.jsonPrimitive?.content
                 ?: throw IllegalStateException("No login token found!")
@@ -72,38 +78,28 @@ object TokenResolver {
             put("email", userName)
         }.toString()
 
-        val response = client.newCall(
+        client.newCall(
             Request.Builder()
                 .url(TOKEN_GATEWAY_URL)
                 .addHeader("Cookie", loginCookie)
                 .post(json.toRequestBody(jsonMediaType))
                 .build()
         ).execute()
-        return createTokenDictionaryFromHeader(response.headers["Set-Cookie"])
     }
 
-    private fun createTokenDictionaryFromHeader(rawCookieHeader: String?): Pair<VRTNUToken, String>? =
-        rawCookieHeader?.let {
-            val cookieDataArr = it.split("X-VRT-Token=")[1].split("; ")
-            val expiresRegex =
-                Regex("([A-Za-z]{3}, \\d{2} [A-Za-z]{3} \\d{4} \\d{2}:\\d{2}:\\d{2} [A-Za-z]{3})")
-            val expires = expiresRegex.find(cookieDataArr[2])!!.groups.first()!!.value
-            Pair(VRTNUToken(cookieDataArr[0]), expires)
-        }
-
     private fun getLoginJson(userName: String, password: String): JsonObject {
-        val requestBody = FormBody.Builder()
-            .add("loginID", userName)
-            .add("password", password)
-            .add("sessionExpiration", "-1")
-            .add("APIKey", API_KEY)
-            .add("targetEnv", "jssdk")
-            .build()
-
         val response = client.newCall(
             Request.Builder()
                 .url(LOGIN_URL)
-                .post(requestBody)
+                .post(
+                    FormBody.Builder()
+                        .add("loginID", userName)
+                        .add("password", password)
+                        .add("sessionExpiration", "-1")
+                        .add("APIKey", API_KEY)
+                        .add("targetEnv", "jssdk")
+                        .build()
+                )
                 .build()
         )
             .execute()
