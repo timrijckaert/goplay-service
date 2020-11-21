05708e3d1a480d9702ff2b585869d45517adf05b
timrijckaert <tim.rijckaert@gmail.com>
2020-11-21 13:31:10 +0100

    VRTNU: do not close the ResponseBody in the extension function. Check for cancellation

--- common/src/main/java/be/tapped/vtmgo/common/okHttpClientExt.kt
+++ common/src/main/java/be/tapped/vtmgo/common/okHttpClientExt.kt
@@ -1,24 +1,21 @@
 package be.tapped.vtmgo.common
 
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import kotlinx.coroutines.suspendCancellableCoroutine
+import okhttp3.*
 import java.io.IOException
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
-import kotlin.coroutines.suspendCoroutine
 
 suspend fun OkHttpClient.executeAsync(request: Request): Response =
-    suspendCoroutine { continuation ->
+    suspendCancellableCoroutine { continuation ->
         newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 continuation.resumeWithException(e)
             }
 
             override fun onResponse(call: Call, response: Response) {
-                response.use { continuation.resume(it) }
+                if (continuation.isCancelled) return
+                continuation.resume(response)
             }
         })
     }
