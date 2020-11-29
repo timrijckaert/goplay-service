001bc55e9899d9facab18cdd345b1ed444baef42
Tim Rijckaert <tim.rijckaert@gmail.com>
2020-11-29 19:23:53 +0100

    VTM GO: Start of fetching live stream channels

--- vtmgo/src/main/java/be/tapped/vtmgo/common/HeaderBuilder.kt
+++ vtmgo/src/main/java/be/tapped/vtmgo/common/HeaderBuilder.kt
@@ -5,13 +5,16 @@ import be.tapped.vtmgo.profile.Profile
 import okhttp3.Headers
 
 interface HeaderBuilder {
+
+    val defaultHeaders: Headers
+
     fun authenticationHeaders(jwt: JWT, profile: Profile? = null): Headers
 }
 
 internal class AuthorizationHeaderBuilder : HeaderBuilder {
 
     // https://github.com/add-ons/plugin.video.vtm.go/wiki/API#vtm-go-api
-    private val defaultHeaders =
+    override val defaultHeaders =
         Headers.Builder()
             .add("x-app-version", "8")
             .add("x-persgroep-mobile-app", "true")
