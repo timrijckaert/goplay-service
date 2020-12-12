85b42cf00b9b34239d7d4b6b248e957e62a07a53
Tim Rijckaert <tim.rijckaert@gmail.com>
2020-12-12 18:26:07 +0100

    VIER: Dirty fix to parse the HeaderVideo

--- vier/src/main/java/be/tapped/vier/content/model.kt
+++ vier/src/main/java/be/tapped/vier/content/model.kt
@@ -38,16 +38,84 @@ public object CustomHeaderVideoSerializer : KSerializer<HeaderVideo?> {
     override fun deserialize(decoder: Decoder): HeaderVideo? {
         val input = decoder as JsonDecoder
         val json = input.decodeJsonElement()
-        return if (json is JsonArray) {
-            null
+        return if (json is JsonObject) {
+            val h = decoder.decodeSerializableValue(HeaderVideo2.serializer())
+            HeaderVideo(
+                autoplay = h.autoplay,
+                cimTag = h.cimTag,
+                createdDate = h.createdDate,
+                description = h.description,
+                duration = h.duration,
+                embedCta = h.embedCta,
+                enablePreroll = h.enablePreroll,
+                episodeNumber = h.episodeNumber,
+                episodeTitle = h.episodeTitle,
+                hasProductPlacement = h.hasProductPlacement,
+                image = h.image,
+                isProtected = h.isProtected,
+                isSeekable = h.isSeekable,
+                isStreaming = h.isStreaming,
+                link = h.link,
+                midrollOffsets = h.midrollOffsets,
+                pageInfo = h.pageInfo,
+                pageUuid = h.pageUuid,
+                parentalRating = h.parentalRating,
+                path = h.path,
+                program = h.program,
+                seasonNumber = h.seasonNumber,
+                seekableFrom = h.seekableFrom,
+                title = h.title,
+                type = h.type,
+                unpublishDate = h.unpublishDate,
+                videoUuid = h.videoUuid,
+                whatsonId = h.whatsonId,
+                needs16PlusLabel = h.needs16PlusLabel,
+                badge = h.badge,
+            )
         } else {
-            //delegate to default generator KSerializer
+            null
         }
     }
 
     override fun serialize(encoder: Encoder, value: HeaderVideo?) {}
 
 }
+
+// Unable to delegate to generated KSerializer
+// This object is a copy of HeaderVideo and it's only purpose is to generate a KSerializer for easy mapping.
+@Serializable
+public data class HeaderVideo2(
+    val autoplay: Boolean,
+    val cimTag: String,
+    val createdDate: Int,
+    val description: String,
+    val duration: Int,
+    val embedCta: JsonPrimitive?,
+    val enablePreroll: Boolean,
+    val episodeNumber: String,
+    val episodeTitle: String?,
+    val hasProductPlacement: Boolean,
+    val image: String,
+    val isProtected: Boolean,
+    val isSeekable: Boolean,
+    val isStreaming: Boolean,
+    val link: String,
+    val midrollOffsets: List<JsonObject>,
+    val pageInfo: PageInfo,
+    val pageUuid: String,
+    val parentalRating: String,
+    val path: String,
+    val program: HeaderProgram,
+    val seasonNumber: String,
+    val seekableFrom: Int,
+    val title: String,
+    val type: String,
+    val unpublishDate: String,
+    val videoUuid: String,
+    val whatsonId: String?,
+    val needs16PlusLabel: Boolean,
+    val badge: String,
+)
 
 @Serializable(with = CustomHeaderVideoSerializer::class)
 public data class HeaderVideo(
