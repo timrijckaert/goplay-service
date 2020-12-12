7e827796e2fd965c15c9900aa3d08d97ef4426b5
Tim Rijckaert <tim.rijckaert@gmail.com>
2020-12-12 19:12:00 +0100

    VIER: Cope with shitty JSON response from VIER api

--- vier/src/main/java/be/tapped/vier/content/model.kt
+++ vier/src/main/java/be/tapped/vier/content/model.kt
@@ -1,25 +1,14 @@
 package be.tapped.vier.content
 
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.buildClassSerialDescriptor
-import kotlinx.serialization.descriptors.serialDescriptor
-import kotlinx.serialization.encoding.CompositeDecoder
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.encoding.decodeStructure
-import kotlinx.serialization.internal.NamedValueDecoder
-import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonDecoder
-import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
-import kotlin.reflect.KCallable
 
 @Serializable
 public data class Images(
@@ -29,9 +18,6 @@ public data class Images(
     val teaser: String,
 )
 
-@Serializable
-public data class HeaderProgram(val title: String, val poster: String)
-
 public object CustomHeaderVideoSerializer : KSerializer<HeaderVideo?> {
     override val descriptor: SerialDescriptor = buildClassSerialDescriptor("HeaderVideo")
 
@@ -39,39 +25,42 @@ public object CustomHeaderVideoSerializer : KSerializer<HeaderVideo?> {
         val input = decoder as JsonDecoder
         val json = input.decodeJsonElement()
         return if (json is JsonObject) {
-            val h = decoder.decodeSerializableValue(HeaderVideo2.serializer())
-            HeaderVideo(
-                autoplay = h.autoplay,
-                cimTag = h.cimTag,
-                createdDate = h.createdDate,
-                description = h.description,
-                duration = h.duration,
-                embedCta = h.embedCta,
-                enablePreroll = h.enablePreroll,
-                episodeNumber = h.episodeNumber,
-                episodeTitle = h.episodeTitle,
-                hasProductPlacement = h.hasProductPlacement,
-                image = h.image,
-                isProtected = h.isProtected,
-                isSeekable = h.isSeekable,
-                isStreaming = h.isStreaming,
-                link = h.link,
-                midrollOffsets = h.midrollOffsets,
-                pageInfo = h.pageInfo,
-                pageUuid = h.pageUuid,
-                parentalRating = h.parentalRating,
-                path = h.path,
-                program = h.program,
-                seasonNumber = h.seasonNumber,
-                seekableFrom = h.seekableFrom,
-                title = h.title,
-                type = h.type,
-                unpublishDate = h.unpublishDate,
-                videoUuid = h.videoUuid,
-                whatsonId = h.whatsonId,
-                needs16PlusLabel = h.needs16PlusLabel,
-                badge = h.badge,
-            )
+            try {
+                val h = decoder.decodeSerializableValue(HeaderVideo2.serializer())
+                HeaderVideo(
+                    autoplay = h.autoplay,
+                    cimTag = h.cimTag,
+                    createdDate = h.createdDate,
+                    description = h.description,
+                    duration = h.duration,
+                    embedCta = h.embedCta,
+                    enablePreroll = h.enablePreroll,
+                    episodeNumber = h.episodeNumber,
+                    episodeTitle = h.episodeTitle,
+                    hasProductPlacement = h.hasProductPlacement,
+                    image = h.image,
+                    isProtected = h.isProtected,
+                    isSeekable = h.isSeekable,
+                    isStreaming = h.isStreaming,
+                    link = h.link,
+                    midrollOffsets = h.midrollOffsets,
+                    pageInfo = h.pageInfo,
+                    pageUuid = h.pageUuid,
+                    parentalRating = h.parentalRating,
+                    path = h.path,
+                    seasonNumber = h.seasonNumber,
+                    seekableFrom = h.seekableFrom,
+                    title = h.title,
+                    type = h.type,
+                    unpublishDate = h.unpublishDate,
+                    videoUuid = h.videoUuid,
+                    whatsonId = h.whatsonId,
+                    needs16PlusLabel = h.needs16PlusLabel,
+                    badge = h.badge,
+                )
+            } catch (e: Exception) {
+                null
+            }
         } else {
             null
         }
@@ -100,12 +89,11 @@ public data class HeaderVideo2(
     val isSeekable: Boolean,
     val isStreaming: Boolean,
     val link: String,
-    val midrollOffsets: List<JsonObject>,
+    val midrollOffsets: List<Int>,
     val pageInfo: PageInfo,
     val pageUuid: String,
     val parentalRating: String,
     val path: String,
-    val program: HeaderProgram,
     val seasonNumber: String,
     val seekableFrom: Int,
     val title: String,
@@ -113,8 +101,8 @@ public data class HeaderVideo2(
     val unpublishDate: String,
     val videoUuid: String,
     val whatsonId: String?,
-    val needs16PlusLabel: Boolean,
-    val badge: String,
+    val needs16PlusLabel: Boolean? = null,
+    val badge: String? = null,
 )
 
 @Serializable(with = CustomHeaderVideoSerializer::class)
@@ -134,12 +122,11 @@ public data class HeaderVideo(
     val isSeekable: Boolean,
     val isStreaming: Boolean,
     val link: String,
-    val midrollOffsets: List<JsonObject>,
+    val midrollOffsets: List<Int>,
     val pageInfo: PageInfo,
     val pageUuid: String,
     val parentalRating: String,
     val path: String,
-    val program: HeaderProgram,
     val seasonNumber: String,
     val seekableFrom: Int,
     val title: String,
@@ -147,8 +134,8 @@ public data class HeaderVideo(
     val unpublishDate: String,
     val videoUuid: String,
     val whatsonId: String?,
-    val needs16PlusLabel: Boolean,
-    val badge: String,
+    val needs16PlusLabel: Boolean? = null,
+    val badge: String? = null,
 )
 
 @Serializable
@@ -185,6 +172,48 @@ public data class Social(
 )
 
 @Serializable
+public data class Episode(
+    val autoplay: Boolean,
+    val badge: String? = null,
+    val cimTag: String,
+    val createdDate: Int,
+    val description: String,
+    val duration: Int,
+    val embedCta: JsonPrimitive?,
+    val enablePreroll: Boolean,
+    val episodeNumber: String,
+    val episodeTitle: String,
+    val hasProductPlacement: Boolean,
+    val image: String,
+    val isProtected: Boolean,
+    val isSeekable: Boolean,
+    val isStreaming: Boolean,
+    val link: String,
+    val midrollOffsets: List<Int>,
+    val needs16PlusLabel: Boolean? = null,
+    val pageInfo: PageInfo,
+    val pageUuid: String,
+    val parentalRating: String,
+    val path: String,
+    val seasonNumber: String,
+    val seekableFrom: Int,
+    val title: String,
+    val type: String,
+    val unpublishDate: String,
+    val videoUuid: String,
+    val whatsonId: String,
+)
+
+@Serializable
+public data class Playlist(
+    val episodes: List<Episode>,
+    val id: String,
+    val link: String,
+    val pageInfo: PageInfo,
+    val title: String,
+)
+
+@Serializable
 public data class Program(
     val id: String,
     val title: String,
@@ -195,6 +224,6 @@ public data class Program(
     val images: Images,
     val header: Header,
     val pageInfo: PageInfo,
-    val playlists: List<JsonObject>,
+    val playlists: List<Playlist>,
     val social: Social,
 )
