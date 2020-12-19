28322158bb5dc3eaadae81a0fcc4cf5224513820
Tim Rijckaert <tim.rijckaert@gmail.com>
2020-12-19 13:24:55 +0100

    VRT NU: ElasticSearch test turbine?

--- vrtnu/src/test/java/be/tapped/vrtnu/content/HttpEpisodeRepoTest.kt
+++ vrtnu/src/test/java/be/tapped/vrtnu/content/HttpEpisodeRepoTest.kt
@@ -1,24 +1,18 @@
 package be.tapped.vrtnu.content
 
-import arrow.core.Either
-import be.tapped.vrtnu.ApiResponse
+import app.cash.turbine.test
 import be.tapped.vrtnu.common.defaultOkHttpClient
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.collections.shouldNotBeEmpty
-import io.kotest.matchers.should
-import io.kotest.matchers.types.beOfType
-import kotlinx.coroutines.flow.toList
+import kotlin.time.ExperimentalTime
 
+@OptIn(ExperimentalTime::class)
 public class HttpEpisodeRepoTest : StringSpec({
 
     val episodeRepo = HttpEpisodeRepo(defaultOkHttpClient, JsonEpisodeParser())
 
     "episodes for default search query" {
-        val toList = episodeRepo.episodes(ElasticSearchQueryBuilder.SearchQuery()).toList()
-        toList
-            .forEach {
-                it should beOfType<Either.Right<ApiResponse.Success.Content.Episodes>>()
-                it.orNull()!!.episodes.shouldNotBeEmpty()
-            }
+        episodeRepo.episodes(ElasticSearchQueryBuilder.SearchQuery()).test {
+            expectComplete()
+        }
     }
 })
