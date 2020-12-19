90fe7408bb450a5596d4252320d7c686cd61ec2f
Tim Rijckaert <tim.rijckaert@gmail.com>
2020-12-19 16:23:25 +0100

    VRT NU: Episode parsing

---
+++ vrtnu/src/test/java/be/tapped/vrtnu/content/JsonEpisodeParserTest.kt
@@ -0,0 +1,25 @@
+package be.tapped.vrtnu.content
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+
+public class JsonEpisodeParserTest : StringSpec({
+
+    "should be able to parse the episodes" {
+        val episodeJson = javaClass.classLoader?.getResourceAsStream("episodes.json")!!.reader().readText()
+        val searchResult = JsonEpisodeParser().parse(episodeJson).orNull()!!
+        searchResult.results shouldHaveSize 22
+        searchResult.meta shouldBe Meta(22, Pages(1, 1, 150))
+        searchResult.facets shouldBe
+            FacetWrapper(
+                listOf(
+                    Facet("seasons", listOf(Bucket("2020", 22))),
+                    Facet("brands", listOf(Bucket("canvas", 22))),
+                    Facet("programs", listOf(Bucket("Terzake", 22))),
+                    Facet("categories", listOf(Bucket("nieuws-en-actua", 22), Bucket("talkshows", 22))),
+                )
+            )
+    }
+
+})
--- vrtnu/src/test/java/be/tapped/vrtnu/content/JsonProgramParserTest.kt
+++ vrtnu/src/test/java/be/tapped/vrtnu/content/JsonProgramParserTest.kt
@@ -3,13 +3,11 @@ package be.tapped.vrtnu.content
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldHaveSize
 
-public class JsonProgramParserTest : StringSpec() {
+public class JsonProgramParserTest : StringSpec({
 
-    init {
-        "should be able to parse the programs" {
-            val programJson = javaClass.classLoader?.getResourceAsStream("programs.json")!!.reader().readText()
-            val programs = JsonProgramParser().parse(programJson).orNull()!!
-            programs shouldHaveSize 495
-        }
+    "should be able to parse the programs" {
+        val programJson = javaClass.classLoader?.getResourceAsStream("programs.json")!!.reader().readText()
+        val programs = JsonProgramParser().parse(programJson).orNull()!!
+        programs shouldHaveSize 495
     }
-}
+})
