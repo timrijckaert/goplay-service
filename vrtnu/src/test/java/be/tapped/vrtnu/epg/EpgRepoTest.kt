e3625f924944bd1ee0373949ef08c145cd83e073
timrijckaert <tim.rijckaert@gmail.com>
2020-12-20 18:32:57 +0100

    VRT NU: EPG E2E tests

---
+++ vrtnu/src/test/java/be/tapped/vrtnu/epg/EpgRepoTest.kt
@@ -0,0 +1,34 @@
+package be.tapped.vrtnu.epg
+
+import io.kotest.assertions.arrow.either.shouldBeRight
+import io.kotest.core.spec.style.StringSpec
+import java.util.*
+
+public class EpgRepoTest : StringSpec() {
+    private val epg = HttpEpgRepo()
+    private val today get() = Calendar.getInstance()
+
+    init {
+        "should be able to retrieve EPG for today" {
+            epg.epg(today).shouldBeRight()
+        }
+
+        (1..30).forEach { days ->
+            "should be able to retrieve EPG for $days days back in time" {
+                val dateInThePast = today.apply {
+                    add(Calendar.DAY_OF_MONTH, -days)
+                }
+                epg.epg(dateInThePast).shouldBeRight()
+            }
+        }
+
+        (1..14).forEach { days ->
+            "should be able to retrieve EPG for $days days in the future" {
+                val dateInThePast = today.apply {
+                    add(Calendar.DAY_OF_MONTH, days)
+                }
+                epg.epg(dateInThePast).shouldBeRight()
+            }
+        }
+    }
+}
