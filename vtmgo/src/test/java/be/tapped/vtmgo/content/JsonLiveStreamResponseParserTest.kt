5497877e9a3d4a061d5d6e30ec22629f2ac3ac12
timrijckaert <tim.rijckaert@gmail.com>
2021-01-02 14:23:02 +0100

    VTM GO: Update JsonStreamResponseParser tests for LIVE and VOD

--- vtmgo/src/test/java/be/tapped/vtmgo/content/JsonLiveStreamResponseParserTest.kt
+++ vtmgo/src/test/java/be/tapped/vtmgo/content/JsonLiveStreamResponseParserTest.kt
@@ -5,9 +5,15 @@ import io.kotest.matchers.shouldNotBe
 
 public class JsonLiveStreamResponseParserTest : StringSpec() {
     init {
-        "should be able to parse" {
+        "should be able to parse live stream response" {
             val liveStreamJson = javaClass.classLoader?.getResourceAsStream("live-stream.json")!!.reader().readText()
             val streamResponse = JsonLiveStreamResponseParser().parse(liveStreamJson)
+            streamResponse.orNull() shouldNotBe null
+        }
+
+        "should be able to parse vod stream response" {
+            val vodStreamJson = javaClass.classLoader?.getResourceAsStream("vod-stream.json")!!.reader().readText()
+            val streamResponse = JsonLiveStreamResponseParser().parse(vodStreamJson)
             streamResponse.orNull() shouldNotBe null
         }
     }
