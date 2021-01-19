1e6f98eb021177f48f270222c88a5ebfeb8cb878
Tim Rijckaert <tim.rijckaert@gmail.com>
2021-01-19 14:57:46 +0100

    VRT NU: Only EEN, CANVAS and KETNET support the screenshot API

--- vrtnu/src/main/java/be/tapped/vrtnu/content/ScreenshotRepo.kt
+++ vrtnu/src/main/java/be/tapped/vrtnu/content/ScreenshotRepo.kt
@@ -1,10 +1,25 @@
 package be.tapped.vrtnu.content
 
 public interface ScreenshotRepo {
-    public fun screenshotForBrand(brandName: String): String
+    public enum class Brand {
+        EEN,
+        CANVAS,
+        KETNET;
+    }
+
+    public fun screenshotForBrand(brand: Brand): String
 }
 
 public object DefaultScreenshotRepo : ScreenshotRepo {
+
     private const val BASE_SCREENSHOT_URL = "https://vrtnu-api.vrt.be/screenshots"
-    override fun screenshotForBrand(brandName: String): String = "$BASE_SCREENSHOT_URL/${brandName.toLowerCase()}.jpg"
+
+    private fun convertBrandToUrlPath(brand: ScreenshotRepo.Brand): String =
+        when (brand) {
+            ScreenshotRepo.Brand.EEN -> "een"
+            ScreenshotRepo.Brand.CANVAS -> "canvas"
+            ScreenshotRepo.Brand.KETNET -> "ketnet"
+        }
+
+    override fun screenshotForBrand(brand: ScreenshotRepo.Brand): String = "$BASE_SCREENSHOT_URL/${convertBrandToUrlPath(brand)}.jpg"
 }
