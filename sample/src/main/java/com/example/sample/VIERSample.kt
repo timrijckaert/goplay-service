f13f5fe42d7f3f7c9cf2e34282bfa29655307020
Tim Rijckaert <tim.rijckaert@bnpparibasfortis.com>
2021-01-30 13:05:36 +0100

    Improve GoPlay sample

--- sample/src/main/java/com/example/sample/VIERSample.kt
+++ sample/src/main/java/com/example/sample/VIERSample.kt
@@ -2,11 +2,9 @@ package com.example.sample
 
 import arrow.core.Tuple2
 import arrow.core.toT
+import arrow.fx.coroutines.parTraverse
 import be.tapped.goplay.ApiResponse
-import be.tapped.goplay.content.EpisodeUuid
-import be.tapped.goplay.content.SearchHit
-import be.tapped.goplay.content.VideoUuid
-import be.tapped.goplay.content.GoPlayApi
+import be.tapped.goplay.content.*
 import be.tapped.goplay.epg.HttpEpgRepo
 import be.tapped.goplay.profile.HttpProfileRepo
 
@@ -31,37 +29,50 @@ private suspend fun api(token: ApiResponse.Success.Authentication.Token) {
     val programs = fetchPrograms.orNull()!!
     println(programs)
 
+    val episodesOfFirstProgram: List<Program.Playlist.Episode> = programs.programs.first().playlists.flatMap(Program.Playlist::episodes)
+    println(episodesOfFirstProgram)
+    val episodeVideoUuids = episodesOfFirstProgram.map(Program.Playlist.Episode::id)
+    println(episodeVideoUuids)
+
+    val episodeStreams = episodeVideoUuids.parTraverse { vierApi.streamByVideoUuid(token.token.idToken, it) }
+    println(episodeStreams)
+
     // Program by URL
     val deSlimsteMensTerWereldByProgramUrl =
-        vierApi.fetchProgram(SearchHit.Source.SearchKey.Program("https://www.goplay.be/de-slimste-mens-ter-wereld"))
+            vierApi.fetchProgram(SearchHit.Source.SearchKey.Program("https://www.goplay.be/de-slimste-mens-ter-wereld"))
     println(deSlimsteMensTerWereldByProgramUrl)
 
     // Search
-    val deSlimsteMensSearchQuery = vierApi.search("de slimste mens ter wereld")
-    println(deSlimsteMensSearchQuery)
+    val searchResults = vierApi.search("big brother")
+    println(searchResults)
 
-    // Episode by URL
-    val deSlimsteMensS18E36ByUrl = vierApi.fetchEpisode(
-        SearchHit.Source.SearchKey.EpisodeByNodeId(
-            "35399",
-            "https://www.vier.be/video/de-slimste-mens-ter-wereld/de-slimste-mens-ter-wereld-s18/de-slimste-mens-ter-wereld-s18-aflevering-36"
-        )
-    )
-    println(deSlimsteMensS18E36ByUrl)
+    if (searchResults.isRight()) {
+        searchResults.orNull()!!.hits
+                .filter { it.source.bundle == SearchHit.Source.Bundle.PROGRAM || it.source.bundle == SearchHit.Source.Bundle.VIDEO }
+                .groupBy { it.source.bundle }
+                .forEach { (bundle, searchHits) ->
+                    if (bundle == SearchHit.Source.Bundle.PROGRAM) {
+                        val programsFromSearchHits = searchHits
+                                .map { it.source.searchKey as SearchHit.Source.SearchKey.Program }
+                                .parTraverse(vierApi::fetchProgram)
+                        println(programsFromSearchHits)
+                    }
+                    if (bundle == SearchHit.Source.Bundle.VIDEO) {
+                        val episodesFromSearchHits = searchHits.map { it.source.searchKey as SearchHit.Source.SearchKey.EpisodeByNodeId }
+                                .parTraverse(vierApi::fetchEpisode)
+                        println(episodesFromSearchHits)
+                    }
+                }
+    }
 
     // Episode by Episode UUID
     val episode = vierApi.fetchEpisode(EpisodeUuid("c4e8d653-224a-4985-95ac-c8360074c518"))
     println(episode)
-
-    // Stream
-    val s18e36 = VideoUuid("26ab85f9-3946-4e1b-8b3f-79018252acb0")
-    val deSlimsteMensS18E36Stream = vierApi.streamByVideoUuid(token.token.idToken, s18e36)
-    println(deSlimsteMensS18E36Stream)
 }
 
 private suspend fun authentication(
-    userName: String,
-    password: String,
+        userName: String,
+        password: String,
 ): Tuple2<ApiResponse.Success.Authentication.Token, ApiResponse.Success.Authentication.Profile> {
     val httpProfileRepo = HttpProfileRepo()
     val token = httpProfileRepo.fetchTokens(userName, password).orNull()!!
