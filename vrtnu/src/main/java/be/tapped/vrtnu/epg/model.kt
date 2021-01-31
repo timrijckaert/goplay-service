7f0c88457a95ced0c0aa7f337171be38df9c30ee
Tim Rijckaert <tim.rijckaert@bnpparibasfortis.com>
2021-01-31 18:35:50 +0100

    Make data classes that are only used by the Epg part of the data class

--- vrtnu/src/main/java/be/tapped/vrtnu/epg/model.kt
+++ vrtnu/src/main/java/be/tapped/vrtnu/epg/model.kt
@@ -5,34 +5,34 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 public data class Epg(
-    @SerialName("O8") val een: List<EpgProgram>,
-    @SerialName("1H") val canvas: List<EpgProgram>,
-    @SerialName("O9") val ketnet: List<EpgProgram>,
-)
-
-@Serializable
-public data class EpgProgram(
-    val start: String,
-    val end: String,
-    val startTime: String,
-    val endTime: String,
-    val title: String,
-    @SerialName("vrt.whatson-id") val whatsonId: String? = null,
-    val image: String? = null,
-    val programWhatsonId: String? = null,
-    val featured: Boolean? = null,
-    val description: String? = null,
-    val shortDescription: String? = null,
-    val url: String? = null,
-    val programPath: String? = null,
-    val assetPath: String? = null,
-    val subtitle: String? = null,
+        @SerialName("O8") val een: List<EpgProgram>,
+        @SerialName("1H") val canvas: List<EpgProgram>,
+        @SerialName("O9") val ketnet: List<EpgProgram>,
 ) {
-    init {
-        require(start.isNotEmpty()) { "start can not be blank." }
-        require(end.isNotEmpty()) { "end can not be blank." }
-        require(startTime.isNotEmpty()) { "startTime can not be blank." }
-        require(endTime.isNotEmpty()) { "endTime can not be blank." }
-        require(title.isNotEmpty()) { "title can not be blank." }
+    @Serializable
+    public data class EpgProgram(
+            val start: String,
+            val end: String,
+            val startTime: String,
+            val endTime: String,
+            val title: String,
+            @SerialName("vrt.whatson-id") val whatsonId: String? = null,
+            val image: String? = null,
+            val programWhatsonId: String? = null,
+            val featured: Boolean? = null,
+            val description: String? = null,
+            val shortDescription: String? = null,
+            val url: String? = null,
+            val programPath: String? = null,
+            val assetPath: String? = null,
+            val subtitle: String? = null,
+    ) {
+        init {
+            require(start.isNotEmpty()) { "start can not be blank." }
+            require(end.isNotEmpty()) { "end can not be blank." }
+            require(startTime.isNotEmpty()) { "startTime can not be blank." }
+            require(endTime.isNotEmpty()) { "endTime can not be blank." }
+            require(title.isNotEmpty()) { "title can not be blank." }
+        }
     }
 }
