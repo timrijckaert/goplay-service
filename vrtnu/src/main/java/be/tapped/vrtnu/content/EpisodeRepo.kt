f47720269312ab69d5347a2b1cc07585ea133f04
Tim Rijckaert <tim.rijckaert@bnpparibasfortis.com>
2021-01-31 16:37:32 +0100

    Episode reformat

--- vrtnu/src/main/java/be/tapped/vrtnu/content/EpisodeRepo.kt
+++ vrtnu/src/main/java/be/tapped/vrtnu/content/EpisodeRepo.kt
@@ -23,8 +23,8 @@ internal class JsonEpisodeParser(private val urlPrefixMapper: UrlPrefixMapper) {
         return Either.catch { Json.decodeFromString<ElasticSearchResult<Episode>>(json) }.map { elasticSearch ->
             elasticSearch.copy(results = elasticSearch.results.map {
                 it.copy(
-                    programImageUrl = urlPrefixMapper.toHttpsUrl(it.programImageUrl),
-                    videoThumbnailUrl = urlPrefixMapper.toHttpsUrl(it.videoThumbnailUrl),
+                        programImageUrl = urlPrefixMapper.toHttpsUrl(it.programImageUrl),
+                        videoThumbnailUrl = urlPrefixMapper.toHttpsUrl(it.videoThumbnailUrl),
                 )
             })
         }.mapLeft(::JsonParsingException)
@@ -36,35 +36,35 @@ public interface EpisodeRepo {
     public fun episodes(searchQuery: ElasticSearchQueryBuilder.SearchQuery): Flow<Either<ApiResponse.Failure, ApiResponse.Success.Content.Episodes>>
 
     public fun episodesForProgram(program: Program): Flow<Either<ApiResponse.Failure, ApiResponse.Success.Content.Episodes>> =
-        episodes(ElasticSearchQueryBuilder.SearchQuery(programName = program.programName))
+            episodes(ElasticSearchQueryBuilder.SearchQuery(programName = program.programName))
 
     public fun fetchMostRecent(): Flow<Either<ApiResponse.Failure, ApiResponse.Success.Content.Episodes>> = episodes(
-        ElasticSearchQueryBuilder.SearchQuery(
-            size = 25,
-            custom = mapOf("allowedRegion" to "BE,WORLD", "brands" to "een,canvas,klara,mnm,radio1,radio2,sporza,stubru,vrtnws,vrtnu,vrtnxt"),
-        )
+            ElasticSearchQueryBuilder.SearchQuery(
+                    size = 25,
+                    custom = mapOf("allowedRegion" to "BE,WORLD", "brands" to "een,canvas,klara,mnm,radio1,radio2,sporza,stubru,vrtnws,vrtnu,vrtnxt"),
+            )
     )
 }
 
 internal class HttpEpisodeRepo(
-    private val client: OkHttpClient,
-    private val jsonEpisodeParser: JsonEpisodeParser,
+        private val client: OkHttpClient,
+        private val jsonEpisodeParser: JsonEpisodeParser,
 ) : EpisodeRepo {
 
     override fun episodes(searchQuery: ElasticSearchQueryBuilder.SearchQuery): Flow<Either<ApiResponse.Failure, ApiResponse.Success.Content.Episodes>> =
-        unfoldFlow(searchQuery.pageIndex) { index ->
-            withContext(Dispatchers.IO) {
-                val episodeByCategoryResponse =
-                    client.executeAsync(Request.Builder().get().url(!constructUrl(searchQuery.copy(pageIndex = index))).build())
+            unfoldFlow(searchQuery.pageIndex) { index ->
+                withContext(Dispatchers.IO) {
+                    val episodeByCategoryResponse =
+                            client.executeAsync(Request.Builder().get().url(!constructUrl(searchQuery.copy(pageIndex = index))).build())
 
-                val searchResultEpisodes = !jsonEpisodeParser.parse(!episodeByCategoryResponse.safeBodyString())
-                if (index != searchQuery.pageIndex && index >= searchResultEpisodes.meta.pages.total) null
-                else Pair(index + 1, ApiResponse.Success.Content.Episodes(searchResultEpisodes.results))
+                    val searchResultEpisodes = !jsonEpisodeParser.parse(!episodeByCategoryResponse.safeBodyString())
+                    if (index != searchQuery.pageIndex && index >= searchResultEpisodes.meta.pages.total) null
+                    else Pair(index + 1, ApiResponse.Success.Content.Episodes(searchResultEpisodes.results))
+                }
             }
-        }
 
     private fun constructUrl(searchQuery: ElasticSearchQueryBuilder.SearchQuery) =
-        HttpUrl.Builder().scheme("https").host("vrtnu-api.vrt.be").addPathSegment("search").applySearchQuery(searchQuery).map(HttpUrl.Builder::build)
+            HttpUrl.Builder().scheme("https").host("vrtnu-api.vrt.be").addPathSegment("search").applySearchQuery(searchQuery).map(HttpUrl.Builder::build)
 
     private fun <A, B, E> unfoldFlow(initialA: A, next: suspend EitherEffect<E, *>.(A) -> Pair<A, B>?): Flow<Either<E, B>> = flow {
         var initial: A? = initialA
