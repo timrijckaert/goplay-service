29d6b585d58ebeb2d034f27760c39b917ead162a
Tim Rijckaert <tim.rijckaert@bnpparibasfortis.com>
2021-02-07 17:44:02 +0100

    VIER: Reformat

--- goplay/src/main/java/be/tapped/goplay/profile/model.kt
+++ goplay/src/main/java/be/tapped/goplay/profile/model.kt
@@ -6,21 +6,21 @@ public inline class IdToken(public val token: String)
 public inline class Expiry(public val dateInMillis: Long)
 
 public data class TokenWrapper(
-    val accessToken: AccessToken,
-    val expiry: Expiry,
-    val tokenType: String,
-    val refreshToken: RefreshToken,
-    val idToken: IdToken,
+        val accessToken: AccessToken,
+        val expiry: Expiry,
+        val tokenType: String,
+        val refreshToken: RefreshToken,
+        val idToken: IdToken,
 )
 
 public data class Profile(
-    val username: String,
-    val sub: String? = null,
-    val birthDate: String? = null,
-    val gender: String? = null,
-    val postalCode: String? = null,
-    val selligentId: String? = null,
-    val name: String? = null,
-    val familyName: String? = null,
-    val email: String? = null,
+        val username: String,
+        val sub: String? = null,
+        val birthDate: String? = null,
+        val gender: String? = null,
+        val postalCode: String? = null,
+        val selligentId: String? = null,
+        val name: String? = null,
+        val familyName: String? = null,
+        val email: String? = null,
 )
