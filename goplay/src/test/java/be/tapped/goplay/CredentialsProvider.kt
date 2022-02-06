ebb02098a1cd5c78cd955582c83de27dafdf84ce
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-02-06 17:57:41 +0100

    Use getenv

--- goplay/src/test/java/be/tapped/goplay/CredentialsProvider.kt
+++ goplay/src/test/java/be/tapped/goplay/CredentialsProvider.kt
@@ -5,10 +5,10 @@ internal object CredentialsProvider {
     private const val PASSWORD_SYSTEM_ENV_KEY = "goplay.password"
 
     val credentials: TestCredentials by lazy {
-        val username = requireNotNull(System.getProperty(USERNAME_SYSTEM_ENV_KEY)) {
+        val username = requireNotNull(System.getenv(USERNAME_SYSTEM_ENV_KEY)) {
             "No GoPlay username found. Be sure to add it to the System environments. $USERNAME_SYSTEM_ENV_KEY"
         }
-        val password = requireNotNull(System.getProperty(PASSWORD_SYSTEM_ENV_KEY)) {
+        val password = requireNotNull(System.getenv(PASSWORD_SYSTEM_ENV_KEY)) {
             "No GoPlay password found. Be sure to add it to the System environments. $PASSWORD_SYSTEM_ENV_KEY"
         }
 
