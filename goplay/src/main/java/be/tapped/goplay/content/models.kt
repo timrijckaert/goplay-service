b3c2d0faf91b06e2c366ba32409d97660a31ea8f
Tim Rijckaert <tim.rijckaert@bnpparibasfortis.com>
2022-02-12 15:41:03 +0100

    Rename `Program` to `ProgramOverview` to make the distinction from a `ProgramDetail`

--- goplay/src/main/java/be/tapped/goplay/content/models.kt
+++ goplay/src/main/java/be/tapped/goplay/content/models.kt
@@ -1,22 +1,13 @@
 package be.tapped.goplay.content
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@JvmInline
-public value class EpisodeUuid(public val id: String)
-
-@JvmInline
-public value class VideoUuid(public val id: String)
-
-@JvmInline
-public value class M3U8Stream(public val url: String)
-
+//<editor-fold desc="Overview">
 /**
  * @param label: Most of time this is an empty string. Sometimes it contains values such as "Volledig seizoen", "nieuw", "Volledige reeks"
  */
 @Serializable
-public data class Program(
+public data class ProgramOverview(
     val id: Id,
     val link: Link,
     val title: String,
@@ -49,63 +40,5 @@ public data class Program(
             GoPlay;
         }
     }
-}
-
-//<editor-fold desc="Search">
-@Serializable
-public data class SearchHit(
-    @SerialName("_index") val index: String,
-    @SerialName("_type") val type: String,
-    @SerialName("_id") val id: String,
-    @SerialName("_score") val score: Double,
-    @SerialName("_source") val source: Source,
-    val highlight: Highlight? = null,
-) {
-    @Serializable
-    public data class Source(
-        val id: String,
-        val type: String,
-        val bundle: Bundle,
-        private val url: String,
-        val language: String,
-        val title: String,
-        val site: String,
-        val intro: String,
-        val created: Int,
-        val changed: Int,
-        val body: List<String>,
-        val terms: List<String>,
-        val suggest: String,
-        val program: String,
-        val img: String,
-        val videos: Int? = null,
-        val duration: Int? = null,
-    ) {
-
-        @Serializable
-        public enum class Bundle {
-            @SerialName("program")
-            PROGRAM,
-
-            @SerialName("video")
-            VIDEO,
-
-            @SerialName("stub")
-            STUB,
-
-            @SerialName("article")
-            ARTICLE,
-
-            @SerialName("playlist")
-            PLAYLIST
-        }
-    }
-
-    @Serializable
-    public data class Highlight(
-        val title: List<String> = emptyList(),
-        val intro: List<String> = emptyList(),
-        val body: List<String> = emptyList(),
-    )
 }
 //</editor-fold>
