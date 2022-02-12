33cf36d9c6e1301109ba3452e30d9db7beacc374
Tim Rijckaert <tim.rijckaert@bnpparibasfortis.com>
2022-02-12 16:38:35 +0100

    Remodel the `Program` so they are part of a sealed hierarchy

--- goplay/src/main/java/be/tapped/goplay/content/models.kt
+++ goplay/src/main/java/be/tapped/goplay/content/models.kt
@@ -2,38 +2,38 @@ package be.tapped.goplay.content
 
 import kotlinx.serialization.Serializable
 
-// TODO discuss the duplication here
+public sealed interface Program {
+    public val id: Id
 
-//<editor-fold desc="Overview">
-/**
- * @param label: Most of time this is an empty string. Sometimes it contains values such as "Volledig seizoen", "nieuw", "Volledige reeks"
- */
-@Serializable
-public data class ProgramOverview(
-    val id: Id,
-    val link: Link,
-    val title: String,
-    val label: String,
-    val pageInfo: PageInfo,
-    val images: Images,
-) {
+    /**
+     * Prefixed by '/'
+     */
+    public val link: Link
+    public val title: String
+
+    /**
+     * Most of time this is an empty string. Sometimes it contains values such as "Volledig seizoen", "nieuw", "Volledige reeks"
+     */
+    public val label: String
+    public val pageInfo: PageInfo
+    public val images: Images
 
     @Serializable
     @JvmInline
     public value class Id(public val id: String)
 
-    /**
-     * @param link: Prefixed by '/'
-     */
     @Serializable
     @JvmInline
     public value class Link(public val link: String)
 
-    @Serializable
-    public data class Images(val poster: String, val teaser: String)
+    public sealed interface Images {
+        public val poster: String
+        public val teaser: String
+    }
 
-    @Serializable
-    public data class PageInfo(val brand: Brand) {
+    public sealed interface PageInfo {
+        public val brand: Brand
+
         public enum class Brand {
             Play4,
             Play5,
@@ -42,53 +42,46 @@ public data class ProgramOverview(
             GoPlay;
         }
     }
-}
-//</editor-fold>
-
-//<editor-fold desc="Detail">
-/**
- * @param label: Most of time this is an empty string. Sometimes it contains values such as "Volledig seizoen", "nieuw", "Volledige reeks"
- */
-@Serializable
-public data class ProgramDetail(
-    val id: Id,
-    val link: Link,
-    val title: String,
-    val label: String,
-    val description: String,
-    val pageInfo: PageInfo,
-    val images: Images,
-) {
 
     @Serializable
-    @JvmInline
-    public value class Id(public val id: String)
+    public data class Overview(
+        override val id: Id,
+        override val link: Link,
+        override val title: String,
+        override val label: String,
+        override val pageInfo: PageInfo,
+        override val images: Images,
+    ) : Program {
+        @Serializable
+        public data class PageInfo(override val brand: Program.PageInfo.Brand) : Program.PageInfo
 
-    /**
-     * @param link: Prefixed by '/'
-     */
-    @Serializable
-    @JvmInline
-    public value class Link(public val link: String)
-
-    @Serializable
-    public data class Images(
-        val hero: String,
-        val mobile: String,
-        val poster: String,
-        val teaser: String,
-        val moviePoster: String,
-    )
+        @Serializable
+        public data class Images(override val poster: String, override val teaser: String) : Program.Images
+    }
 
     @Serializable
-    public data class PageInfo(val brand: Brand, val publishDate: String) {
-        public enum class Brand {
-            Play4,
-            Play5,
-            Play6,
-            Play7,
-            GoPlay;
-        }
+    public data class Detail(
+        override val id: Id,
+        override val link: Link,
+        override val title: String,
+        override val label: String,
+        val description: String,
+        override val pageInfo: PageInfo,
+        override val images: Images,
+    ) : Program {
+
+        @Serializable
+        public data class Images(
+            val hero: String,
+            val mobile: String,
+            override val poster: String,
+            override val teaser: String,
+            val moviePoster: String,
+        ) : Program.Images
+
+        @Serializable
+        public data class PageInfo(override val brand: Program.PageInfo.Brand, val publishDate: String) : Program.PageInfo
     }
 }
-//</editor-fold>
+
+
