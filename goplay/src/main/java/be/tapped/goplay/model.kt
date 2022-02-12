40afc61d2961d4fc9db3cc919b6c54ebbd2bc965
Tim Rijckaert <tim.rijckaert@bnpparibasfortis.com>
2022-02-12 15:56:35 +0100

    Refactor the Program details to return the `ApiResponse.Success.Content.ProgramDetail`s object.

--- goplay/src/main/java/be/tapped/goplay/model.kt
+++ goplay/src/main/java/be/tapped/goplay/model.kt
@@ -1,44 +1,47 @@
 package be.tapped.goplay
 
 import arrow.core.Nel
+import be.tapped.goplay.content.ProgramDetail
 import be.tapped.goplay.content.ProgramOverview
 import be.tapped.goplay.epg.EpgProgram
 import be.tapped.goplay.profile.TokenWrapper
 import kotlinx.datetime.LocalDate
 
-public sealed class ApiResponse {
-    public sealed class Success : ApiResponse() {
-        public sealed class Authentication : Success() {
-            public data class Token(val token: TokenWrapper) : Authentication()
-            public data class Profile(val profile: be.tapped.goplay.profile.Profile) : Authentication()
+public sealed interface ApiResponse {
+    public sealed interface Success : ApiResponse {
+        public sealed interface Authentication : Success {
+            public data class Token(val token: TokenWrapper) : Authentication
+            public data class Profile(val profile: be.tapped.goplay.profile.Profile) : Authentication
         }
 
-        public sealed class Content : Success() {
-            public data class Programs(val programs: Nel<ProgramOverview>) : Content()
-            public data class ProgramDetail(val program: ProgramOverview) : Content()
+        public sealed interface Content : Success {
+            public sealed interface Program : Content {
+                public data class Overview(val programs: Nel<ProgramOverview>) : Program
+                public data class Detail(val program: ProgramDetail) : Program
+            }
         }
 
-        public data class ProgramGuide(val epg: List<EpgProgram>) : Success()
+        public data class ProgramGuide(val epg: List<EpgProgram>) : Success
     }
 
-    public sealed class Failure : ApiResponse() {
-        public data class JsonParsingException(val throwable: Throwable) : Failure()
-        public data class HTMLJsonExtractionException(val throwable: Throwable) : Failure()
-        public data class Network(val throwable: Throwable) : Failure()
+    public sealed interface Failure : ApiResponse {
+        public data class JsonParsingException(val throwable: Throwable) : Failure
+        public data class HTMLJsonExtractionException(val throwable: Throwable) : Failure
+        public data class Network(val throwable: Throwable) : Failure
 
-        public sealed class Authentication : Failure() {
-            public data class AWS(val statusCode: Int, val statusText: String?) : Authentication()
-            public object Login : Authentication()
-            public object Refresh : Authentication()
-            public object Profile : Authentication()
+        public sealed interface Authentication : Failure {
+            public data class AWS(val statusCode: Int, val statusText: String?) : Authentication
+            public object Login : Authentication
+            public object Refresh : Authentication
+            public object Profile : Authentication
         }
 
-        public sealed class Content : Failure() {
-            public object NoPrograms : Content()
+        public sealed interface Content : Failure {
+            public object NoPrograms : Content
         }
 
-        public sealed class Epg : Failure() {
-            public data class NoEpgDataFor(val date: LocalDate) : Epg()
+        public sealed interface Epg : Failure {
+            public data class NoEpgDataFor(val date: LocalDate) : Epg
         }
     }
 }
