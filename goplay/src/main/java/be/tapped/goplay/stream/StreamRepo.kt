189f706fbad66ebcf00de2c3a084fa54c85b3be8
Tim Rijckaert <tim.rijckaert@bnpparibasfortis.com>
2022-02-13 12:50:28 +0100

    Model the absence of a JSON response to `NoStreamFound`.
    #21

--- goplay/src/main/java/be/tapped/goplay/stream/StreamRepo.kt
+++ goplay/src/main/java/be/tapped/goplay/stream/StreamRepo.kt
@@ -12,6 +12,7 @@ import be.tapped.goplay.profile.IdToken
 import be.tapped.goplay.safeGet
 import io.ktor.client.HttpClient
 import io.ktor.client.request.headers
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
@@ -24,36 +25,41 @@ internal fun interface StreamRepo {
 internal fun httpStreamRepo(client: HttpClient): StreamRepo =
     StreamRepo { videoId, idToken ->
         either {
-            val videoObj =
-                client.safeGet<JsonObject>("$apiVierVijfZes/content/${videoId.videoUuid}") {
+            val videoResponse =
+                client.safeGet<JsonElement>("$apiVierVijfZes/content/${videoId.videoUuid}") {
                     headers { append(HttpHeaders.AUTHORIZATION, idToken.token) }
                 }.bind()
 
-            val resolvedStream = when {
-                videoObj.containsKey("videoDash") ->
-                    catch {
-                        val drmKey = videoObj.getValue("drmKey").jsonObject.getValue("S").jsonPrimitive.content
-                        val drmResponseJson =
-                            client.safeGet<JsonObject>("$apiGoPlay/video/xml/${drmKey}") {
-                                headers { append(HttpHeaders.AUTHORIZATION, idToken.token) }
-                            }.bind()
-                        val auth = catch(drmResponseJson.getValue("auth").jsonPrimitive::content).mapLeft { ApiResponse.Failure.Stream.DrmAuth(drmResponseJson, it) }.bind()
-                        ResolvedStream.MpegDash(
-                            videoId,
-                            videoObj.getValue("videoDash").jsonObject.getValue("S").jsonPrimitive.content,
-                            auth
-                        )
-                    }.mapLeft { ApiResponse.Failure.Stream.MpegDash(videoObj, it) }
-                videoObj.containsKey("video") ->
-                    catch {
-                        ResolvedStream.Hls(
-                            videoId,
-                            videoObj.getValue("video").jsonObject.getValue("S").jsonPrimitive.content
-                        )
-                    }.mapLeft { ApiResponse.Failure.Stream.Hls(videoObj, it) }
-                else -> ApiResponse.Failure.Stream.UnknownStream(videoObj).left()
-            }.bind()
-
-            ApiResponse.Success.Stream(resolvedStream)
+            if (videoResponse is JsonObject) {
+                val videoObj = videoResponse.jsonObject
+                val resolvedStream =
+                    when {
+                        videoObj.containsKey("videoDash") ->
+                            catch {
+                                val drmKey = videoObj.getValue("drmKey").jsonObject.getValue("S").jsonPrimitive.content
+                                val drmResponseJson =
+                                    client.safeGet<JsonObject>("$apiGoPlay/video/xml/${drmKey}") {
+                                        headers { append(HttpHeaders.AUTHORIZATION, idToken.token) }
+                                    }.bind()
+                                val auth = catch(drmResponseJson.getValue("auth").jsonPrimitive::content).mapLeft { ApiResponse.Failure.Stream.DrmAuth(drmResponseJson, it) }.bind()
+                                ResolvedStream.MpegDash(
+                                    videoId,
+                                    videoObj.getValue("videoDash").jsonObject.getValue("S").jsonPrimitive.content,
+                                    auth
+                                )
+                            }.mapLeft { ApiResponse.Failure.Stream.MpegDash(videoObj, it) }
+                        videoObj.containsKey("video") ->
+                            catch {
+                                ResolvedStream.Hls(
+                                    videoId,
+                                    videoObj.getValue("video").jsonObject.getValue("S").jsonPrimitive.content
+                                )
+                            }.mapLeft { ApiResponse.Failure.Stream.Hls(videoObj, it) }
+                        else -> ApiResponse.Failure.Stream.UnknownStream(videoObj).left()
+                    }.bind()
+                ApiResponse.Success.Stream(resolvedStream)
+            } else {
+                ApiResponse.Success.Stream(ResolvedStream.NoStreamFound(videoId))
+            }
         }
     }
