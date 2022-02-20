a7e542ea98599a92a50206273bbac6d772a3596f
Tim Rijckaert <tim.rijckaert@gmail.com>
2022-02-20 15:57:28 +0100

    Move back to 4spaces for now until we have activated Spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,37 +2,37 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-  id(libs.plugins.kotlin.jvm.pluginId)
+    id(libs.plugins.kotlin.jvm.pluginId)
 }
 
 allprojects {
-  repositories {
-    mavenCentral()
-    google()
-  }
-
-  tasks {
-    withType<KotlinCompile>().configureEach {
-      kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-        freeCompilerArgs = freeCompilerArgs +
-          "-Xexplicit-api=strict" +
-          "-opt-in=kotlinx.serialization.ExperimentalSerializationApi"
-      }
-      sourceCompatibility = JavaVersion.VERSION_11.toString()
-      targetCompatibility = JavaVersion.VERSION_11.toString()
+    repositories {
+        mavenCentral()
+        google()
     }
 
-    withType<Test>().configureEach {
-      useJUnitPlatform()
+    tasks {
+        withType<KotlinCompile>().configureEach {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_11.toString()
+                freeCompilerArgs = freeCompilerArgs +
+                        "-Xexplicit-api=strict" +
+                        "-opt-in=kotlinx.serialization.ExperimentalSerializationApi"
+            }
+            sourceCompatibility = JavaVersion.VERSION_11.toString()
+            targetCompatibility = JavaVersion.VERSION_11.toString()
+        }
 
-      listOf("goplay").forEach { brand ->
-        val username by variable("${brand}.username")
-        val password by variable("${brand}.password")
+        withType<Test>().configureEach {
+            useJUnitPlatform()
 
-        environment("${brand}.username", username)
-        environment("${brand}.password", password)
-      }
+            listOf("goplay").forEach { brand ->
+                val username by variable("${brand}.username")
+                val password by variable("${brand}.password")
+
+                environment("${brand}.username", username)
+                environment("${brand}.password", password)
+            }
+        }
     }
-  }
 }
