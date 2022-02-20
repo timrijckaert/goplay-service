4873b7bb2496933fffb042b3b9404e96c37dca4c
Tim Rijckaert <tim.rijckaert@gmail.com>
2022-02-20 17:01:35 +0100

    Trying to add the logo

--- desktop/src/main/kotlin/be/tapped/goplay/desktop/main.kt
+++ desktop/src/main/kotlin/be/tapped/goplay/desktop/main.kt
@@ -21,8 +21,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import arrow.core.Either
@@ -57,7 +59,8 @@ public suspend fun main() {
             Window(
                 onCloseRequest = ::exitApplication,
                 title = "GoPlay",
-                state = rememberWindowState(width = 600.dp, height = 500.dp)
+                state = rememberWindowState(),
+                icon = painterResource("goplay-logo.svg")
             ) {
                 Box(Modifier.fillMaxSize()) {
                     val lazyListState = rememberLazyListState()
